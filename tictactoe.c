5fd26a7598be58e2f5bb8ece771a5d3639cc72f3
Marlon Martins <marlon673@hotmail.com>
2023-04-11 14:23:09 -0300

    Create tictactoe.c

--- tictactoe.c
+++ tictactoe.c
@@ -2,37 +2,36 @@
 // http://www.comofazerascoisas.com.br/controlando-8-leds-com-arduino-usando-ci74HC595-fazendo-uma-contagem-binaria.html
 // http://www.learningaboutelectronics.com/Articles/Cascade-shift-registers.php
 #include <LiquidCrystal.h>
+
 LiquidCrystal LCD(A4, A5, A0, A1, A2, A3);
 
 int const clock = 2; //ligado ao clock do 74HC595
 int const latch = 3; //ligado ao latch do 74HC595
-int const data  = 4; //ligado ao data do 74HC595
+int const data = 4; //ligado ao data do 74HC595
 
-
-typedef struct _boardCell{
+typedef struct _boardCell {
     int Red;
     int Green;
     int Blue;
 
 } BoardCell;
 
-typedef struct _boardGame{
+typedef struct _boardGame {
     int isBlocked;
     int cellValue; // 0 = vazio; 1 = player; 2 = IA
 } BoardGame;
 
 BoardCell Board[3][3];
 BoardGame boardGame[3][3];
-int playsRemaining = 9;
+int playsRemaining;
+int IADifficulty;
 
 int buttonList[] = {5, 6, 7, 8, 9, 10, 11, 12, 13};
 
-void MasterReset()
-{
-    int qtBits=8*4;
+void MasterReset() {
+    int qtBits = 8 * 4;
     digitalWrite(latch, LOW);
-    for(int i=0; i<qtBits; i++)
-    {
+    for (int i = 0; i < qtBits; i++) {
         digitalWrite(clock, LOW);
         digitalWrite(data, LOW);
         digitalWrite(clock, HIGH);
@@ -41,61 +40,60 @@ void MasterReset()
 
 }
 
-void ResetBoard(int Red, int Green, int Blue)
-{
-    for(int line=0; line<3; line++)
-    {
-        for(int column=0; column<3; column++)
-        {
-            Board[line][column].Red   = Red;
+void ResetBoard(int Red, int Green, int Blue) {
+    for (int line = 0; line < 3; line++) {
+        for (int column = 0; column < 3; column++) {
+            Board[line][column].Red = Red;
             Board[line][column].Green = Green;
-            Board[line][column].Blue  = Blue;
+            Board[line][column].Blue = Blue;
         }
     }
     MasterReset();
 }
 
-void initGameBoard(){
-    for(int line=0; line<3; line++){
-        for(int column=0; column<3; column++){
+void initGameBoard() {
+    ResetBoard(0, 0, 0);
+    playsRemaining = 9;
+    IADifficulty = 0;
+    for (int line = 0; line < 3; line++) {
+        for (int column = 0; column < 3; column++) {
             boardGame[line][column].isBlocked = 0;
             boardGame[line][column].cellValue = 0;
         }
     }
+
+    LCD.clear();
+    LCD.print("Selecione a dificuldade");
 }
 
-void LightBoard()
-{
+void LightBoard() {
     /*
     ** Red -> Blue -> Green
     */
 
-    int pos=0;
-    int bit=0;
+    int pos = 0;
+    int bit = 0;
 
     MasterReset();
 
     digitalWrite(latch, LOW); // enable writting
 
-    for(int k=2; k>=0; k--)
-        for(int line=2; line>=0; line--)
-        {
-            for(int column=2; column>=0; column--)
-            {
+    for (int k = 2; k >= 0; k--)
+        for (int line = 2; line >= 0; line--) {
+            for (int column = 2; column >= 0; column--) {
                 //pos = line * 3 + column;
 
                 // sending the bit
                 digitalWrite(clock, LOW);
-                switch(k)
-                {
+                switch (k) {
                     case 0: // Red
-                        bit = (Board[line][column].Red==1?HIGH:LOW);
+                        bit = (Board[line][column].Red == 1 ? HIGH : LOW);
                         break;
                     case 1: // Blue
-                        bit = (Board[line][column].Blue==1?HIGH:LOW);
+                        bit = (Board[line][column].Blue == 1 ? HIGH : LOW);
                         break;
                     case 2: // Green
-                        bit = (Board[line][column].Green==1?HIGH:LOW);
+                        bit = (Board[line][column].Green == 1 ? HIGH : LOW);
                         break;
                     default:
                         bit = 0;
@@ -113,11 +111,11 @@ void LightBoard()
     Serial.println();
 }
 
-int ButtonRead(){
-    int k=0, buttonState=0, button=-1;
-    for(k=0; k < 9; k++){
+int ButtonRead() {
+    int k = 0, buttonState = 0, button = -1;
+    for (k = 0; k < 9; k++) {
         buttonState = digitalRead(buttonList[k]);
-        if(buttonState == HIGH){
+        if (buttonState == HIGH) {
             LCD.clear();
             LCD.print(buttonList[k]);
             button = buttonList[k];
@@ -129,22 +127,41 @@ int ButtonRead(){
 
 }
 
-void playerMove(int line, int column){
-    if(boardGame[line][column].isBlocked == 0){
+void setDifficulty(int keyPressed){
+    switch (keyPressed) {
+        case 13:
+            IADifficulty = 3;
+            break;
+        case 12:
+            IADifficulty = 2;
+            break;
+        case 11:
+            IADifficulty = 1;
+            break;
+    }
+}
+
+void IATurn() {
+
+}
+
+void playerMove(int line, int column) {
+    if (boardGame[line][column].isBlocked == 0) {
         LCD.clear();
         LCD.print("faca sua jogada");
         playsRemaining--;
         boardGame[line][column].isBlocked = 1;
         boardGame[line][column].cellValue = 1;
-        Board[line][column].Blue=1;
-    }else{
+        Board[line][column].Blue = 1;
+        IATurn();
+    } else {
         LCD.clear();
         LCD.print("Selecione um campo vazio");
     }
 }
 
-void playerTurn(int keyPressed){
-    switch(keyPressed){
+void playerTurn(int keyPressed) {
+    switch (keyPressed) {
         case 07:
             playerMove(0, 2);
             break;
@@ -175,43 +192,99 @@ void playerTurn(int keyPressed){
     }
 }
 
-void setup()
-{
+int checkWin() {
+    for (int line = 0; line < 3; line++) {
+        if (boardGame[line][0].cellValue == boardGame[line][1].cellValue &&
+            boardGame[line][0].cellValue == boardGame[line][2].cellValue &&
+            boardGame[line][0].cellValue != 0) {
+            if (boardGame[line][0].cellValue == 1) {
+                LCD.clear();
+                LCD.print("Voce venceu");
+            }
+            if (boardGame[line][0].cellValue == 2) {
+                LCD.clear();
+                LCD.print("Voce perdeu");
+            }
+            return 1;
+        }
+    }
+    for (int column = 0; column < 3; column++) {
+        if (boardGame[0][column].cellValue == boardGame[1][column].cellValue &&
+            boardGame[0][column].cellValue == boardGame[2][column].cellValue &&
+            boardGame[0][column].cellValue != 0) {
+            if (boardGame[0][column].cellValue == 1) {
+                LCD.clear();
+                LCD.print("Voce venceu");
+            }
+            if (boardGame[0][column].cellValue == 2) {
+                LCD.clear();
+                LCD.print("Voce perdeu");
+            }
+            return 1;
+        }
+    }
+    if (boardGame[0][0].cellValue == boardGame[1][1].cellValue &&
+        boardGame[0][0].cellValue == boardGame[2][2].cellValue &&
+        boardGame[0][0].cellValue != 0) {
+        if (boardGame[0][0].cellValue == 1) {
+            LCD.clear();
+            LCD.print("Voce venceu");
+        }
+        if (boardGame[0][0].cellValue == 2) {
+            LCD.clear();
+            LCD.print("Voce perdeu");
+        }
+        return 1;
+    }
+    if (boardGame[0][2].cellValue == boardGame[1][1].cellValue &&
+        boardGame[0][2].cellValue == boardGame[2][0].cellValue &&
+        boardGame[0][2].cellValue != 0) {
+        if (boardGame[0][2].cellValue == 1) {
+            LCD.clear();
+            LCD.print("Voce venceu");
+        }
+        if (boardGame[0][2].cellValue == 2) {
+            LCD.clear();
+            LCD.print("Voce perdeu");
+        }
+        return 1;
+    }
+    return 0;
+}
+
+void setup() {
     Serial.begin(9600);
     LCD.begin(16, 2);
 
     //definindo os pinos como de saída
-    pinMode(clock,OUTPUT);
-    pinMode(latch,OUTPUT);
-    pinMode(data,OUTPUT);
+    pinMode(clock, OUTPUT);
+    pinMode(latch, OUTPUT);
+    pinMode(data, OUTPUT);
 
-    for(int k=0; k < 9; k++)
-    {
+    for (int k = 0; k < 9; k++) {
         pinMode(buttonList[k], INPUT);
     }
 
-
-    // resetting the Board
-    ResetBoard(0, 0, 0);
     initGameBoard();
-
-    LCD.print("faca sua jogada");
 }
 
-void loop(){
+void loop() {
     int keyPressed = -1;
-    int isPlayerTurn = 0;
 
-//    ResetBoard(0, 0, 0);
-    LightBoard();
-
-
+    delay(500);
     keyPressed = ButtonRead();
-    if(keyPressed!= -1){
-        playerTurn(keyPressed);
+    if (IADifficulty != 0) {
+        if (checkWin() == 0) {
+            if (playsRemaining > 0)
+                if (keyPressed != -1)
+                    playerTurn(keyPressed);
+        } else {
+            if (keyPressed != -1)
+                initGameBoard();
+        }
+    } else {
+        if (keyPressed != -1)
+            setDifficulty(keyPressed);
     }
-//    IATurn();
-
-
     LightBoard();
 }
